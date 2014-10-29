7898fbf6638b5c7f535157639062ddaac6ecfcf3
Rachel Hyman <rachelehyman@gmail.com>
2014-10-29 10:42:48 -0500

    Initial Commit

---
+++ ScrapHeap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ScrapHeap
+//
+//  Created by Rachel Hyman on 10/29/14.
+//  Copyright (c) 2014 Rachel Hyman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
